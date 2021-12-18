c8c68b4315d4d60eaf63864949e0917123b905aa
Franco Gomez <mgomez21699@gmail.com>
2021-12-18 14:26:42 -0300

    creada funcionalidad para mostrar todos los datos ingresados por el usuario

--- tarea-5.js
+++ tarea-5.js
@@ -5,16 +5,42 @@
 // mostrar toda la información junta en un campo de texto
 // Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
 const $saludoUsuario = document.querySelector("#saludoUsuario");
+const $formulario = document.querySelector("form");
 const $primerNombre = document.querySelector("#primerNombre");
+const $leyendaFormulario = document.querySelector("#leyendaFormulario");
+const $edad = document.querySelector("#edad");
+const $botonEnviarFormulario = document.querySelector("#botonEnviarFormulario");
 
 $botonEnviarFormulario.onclick = function () {
     if (!$primerNombre.value || !$edad.value) return;
 
+    const nombreCompleto = unificarNombreUsuario();
     saludarUsuario($primerNombre.value.toLowerCase());
+    mostrarInformacionUsuario(nombreCompleto, $edad.value);
 
     return false;
 };
 
+function unificarNombreUsuario() {
+    let nombreCompleto = "";
+
+    for (let i = 0; i < $formulario.childNodes.length; i++) {
+        if ($formulario.childNodes[i].type === "text" && $formulario.childNodes[i].value !== ""){
+            if (nombreCompleto === "") {
+                nombreCompleto = $formulario.childNodes[i].value
+            } else {
+                nombreCompleto += ` ${$formulario.childNodes[i].value}`
+            }
+        }
+    }
+
+    return nombreCompleto;
+}
+
 function saludarUsuario(primerNombre) {
     $saludoUsuario.textContent = `Bienvenido ${primerNombre}!`;
+}
+
+function mostrarInformacionUsuario(nombreCompleto, edad) {
+    $leyendaFormulario.textContent = `Nombre: ${nombreCompleto}, Edad: ${edad} `;
 }
