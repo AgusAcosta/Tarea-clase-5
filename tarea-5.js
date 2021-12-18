116d35efc252e49679b5c1f85841509330c0ecc6
Franco Gomez <mgomez21699@gmail.com>
2021-12-18 18:25:30 -0300

    Definidas las funciones para determinar el promedio, minimo, maximo y mas repetido entre el array de numeros

--- tarea-5.js
+++ tarea-5.js
@@ -8,17 +8,99 @@
 
 const $lista = document.querySelector("ul")
 
-function darValoresRandomLI() {
-    for (const childNode of $lista.childNodes) {
-        if(childNode.localName === "li"){
-            childNode.textContent = generarNumeroRandom(1000)
+const arrayNumerosLI = []
+
+function inicializar() {
+    darValoresRandomLI()
+    pushearValoresLI(arrayNumerosLI)
+}
+
+inicializar()
+
+function calcularPromedio(array) {
+    let resultado = 0
+
+    for (const numero of array) {
+        resultado += Number(numero)
+    }
+
+    return resultado / array.length
+}
+
+function calcularMinimo(array) {
+    let numeroMinimo = array[0]
+
+    for (const numero of array) {
+        if (numeroMinimo > numero) {
+            numeroMinimo = numero
         }
+    }
+
+    return numeroMinimo
+}
+
+function calcularMaximo(array) {
+    let numeroMaximo = array[0]
+    let masRepetido
+
+    for (const numero of array) {
+        if (numeroMaximo < numero) {
+            numeroMaximo = numero
+        }
+    }
+
+    return numeroMaximo
+}
+
+// Si existe mas de un repetido, con la misma cantidad de repeticiones, devuelve el ultimo en repetirse.
+function calcularMasRepetido(array) {
+    const recuentoRepeticiones = {}
+    let existenRepetidos = false
+
+    for (const numero of array) {
+        if(Boolean(recuentoRepeticiones[numero])) {
+            recuentoRepeticiones[numero] += 1
+            masRepetido = numero
+            existenRepetidos = true
+        } else {
+            recuentoRepeticiones[numero] = 1
+        }
+    }
+
+    if (existenRepetidos) {
+        for (const [llave, entrada] of Object.entries(recuentoRepeticiones)) {
+            if (recuentoRepeticiones[masRepetido] < entrada) {
+                masRepetido = llave
+            }
+        }
+
+        return masRepetido
+    } else {
+        return "No hay repetidos."
     }
 }
 
-darValoresRandomLI()
+console.log(`El promedio es: ${calcularPromedio(arrayNumerosLI)}`)
+console.log(`El número más pequeño es: ${calcularMinimo(arrayNumerosLI)}`)
+console.log(`El número más grande es: ${calcularMaximo(arrayNumerosLI)}`)
+console.log(`El número más frecuente es: ${calcularMasRepetido(arrayNumerosLI)}`)
+
+function darValoresRandomLI() {
+    for (const childNode of $lista.childNodes) {
+        if(childNode.localName === "li"){
+            childNode.textContent = generarNumeroRandom(10)
+        }
+    }
+}
 
 function generarNumeroRandom(numeroMaximo) {
     return Math.floor(Math.random() * numeroMaximo)
 }
 
+function pushearValoresLI(array) {
+    for (const childNode of $lista.childNodes) {
+        if(childNode.localName === "li"){
+            array.push(childNode.textContent)
+        }
+    }
+}
