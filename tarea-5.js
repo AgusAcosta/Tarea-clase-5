b0e2a7db30683c7645668a91be76e91f4adbd79d
Franco Gomez <mgomez21699@gmail.com>
2021-12-18 17:14:18 -0300

    Agregada funcionalidad para dar valores random a los list item, cada vez que la pagina sea recargada

--- tarea-5.js
+++ tarea-5.js
@@ -6,3 +6,19 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+const $lista = document.querySelector("ul")
+
+function darValoresRandomLI() {
+    for (const childNode of $lista.childNodes) {
+        if(childNode.localName === "li"){
+            childNode.textContent = generarNumeroRandom(1000)
+        }
+    }
+}
+
+darValoresRandomLI()
+
+function generarNumeroRandom(numeroMaximo) {
+    return Math.floor(Math.random() * numeroMaximo)
+}
+
